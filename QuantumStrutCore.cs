10a7ea81df8763c96e7765d970d353c1c86445d1
Andy <andy@wtribe.com>
2014-02-22 10:45:00 -0800

    Commented out System.Threading.Tasks lines, which don't appear to be available in KSP's assembly, and appear to be unused in QuantumStrut.

--- QuantumStrutCore.cs
+++ QuantumStrutCore.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 using UnityEngine;
 using KSP.IO;
 
